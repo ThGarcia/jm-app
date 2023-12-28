57b251e38f19b26765cfbdcf4a9aa1990b7f41b1
ThGarcia <garciathiagorafael@gmail.com>
2023-12-28 14:23:17 -0300

    error page prototype - fourth commit

---
+++ src/pages/error/Error.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Menu from "../../components/menu/Menu";
+import "./Error.css";
+
+import img from "../../assets/img/error.png";
+import logo from "../../assets/img/jmlogo.png";
+
+const Error = () => {
+  document.title = "Erro 404";
+  return (
+    <div>
+      <Menu />
+      <div className="error">
+        <img className="error-logo" src={logo} alt="logo" />
+        <img className="error-img" src={img} alt="error" />
+        <div  className="error-text">
+            <p>Erro 404! Você está certo disso?</p>
+            <p>Não encontramos a página que você está procurando...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
