c95ef41bc4d67b17d1c8d19ea661b4ece5396e63
ThGarcia <garciathiagorafael@gmail.com>
2024-01-02 11:19:14 -0300

    menu update - sixth committ

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Menu.css";
 
 const Menu = () => {
+  const [isMenuVisible, setMenuVisible] = useState(true);
+
+  useEffect(() => {
+    let isScrolling;
+    let isMouseOverMenu = false;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const shouldShowMenu = currentScrollPos <= 50 || currentScrollPos < lastScrollPos;
+
+      if (!isMouseOverMenu) {
+        setMenuVisible(shouldShowMenu);
+      }
+
+      clearTimeout(isScrolling);
+
+      isScrolling = setTimeout(() => {
+        setMenuVisible(false);
+      }, 3000);
+
+      lastScrollPos = currentScrollPos;
+    };
+
+    let lastScrollPos = window.scrollY;
+
+    const handleMouseEnter = () => {
+      isMouseOverMenu = true;
+    };
+
+    const handleMouseLeave = () => {
+      isMouseOverMenu = false;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    document.querySelector(".menu").addEventListener("mouseenter", handleMouseEnter);
+    document.querySelector(".menu").addEventListener("mouseleave", handleMouseLeave);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      document.querySelector(".menu").removeEventListener("mouseenter", handleMouseEnter);
+      document.querySelector(".menu").removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
   return (
-    <div className="menu">
+    <div className={`menu ${isMenuVisible ? "visible" : "hidden"}`}>
       <div className="menu-left">
         <a href="/*"><div className="menu-logo"></div></a>
       </div>
@@ -15,9 +59,15 @@ const Menu = () => {
           <a className="menu-button" href="/"><p>Contact</p></a>
         </div>
         <a href="/"><div className="menu-portrait"></div></a>
+        <div className="menu-options">
+          <a href="/"><p>Intro</p></a>
+          <a href="/"><p>Works</p></a>
+          <a href="/"><p>About</p></a>
+          <a href="/"><p>Contact</p></a>
+          </div>
       </div>
     </div>
-  );
+ );
 };
 
 export default Menu;
